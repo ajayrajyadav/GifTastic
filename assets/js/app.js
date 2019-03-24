539dcadac8754f19367e7ac5afceb9ea27eb34fb
ajayraj <ajayraj98@gmail.com>
2019-03-23 23:24:18 -0700

    buildhtml() done

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,9 +2,23 @@ $(document).ready(function () {
     buildHtml();
 
     function buildHtml(){
-        addIntoDiv($("<div>"), $(".container"),true, "", false, "", false, "row", true);
-        addIntoDiv($("<div>"), $("#row"),true, "topics", true, "", false, "", false);
+        // var newDiv = $("<div>");
+        // newDiv.html("Movie Title: ");
+        $(".container").append($("<div>").addClass("row"));
+        $(".row").append($("<div>").addClass("col-lg-9"));
+        $(".col-lg-9").append($("<div>").addClass("display"));
 
+        $(".row").append($("<div>").addClass("col-lg-3"));
+        $(".col-lg-3").append($("<form>").attr("id", "text-form"))
+        $("#text-form").append($("<label>").attr({for: "newTextInput", id: "serachLabel"}));
+        $("#serachLabel").html("Add your search here");
+        $("#text-form").append($("<br>"));
+        $("#text-form").append($("<input>").attr({id: "addSearch", type: "submit", value:"Submit"}));
+
+        // attr( { title:"Test", alt:"Test2" } )
+
+        // addIntoDiv($("<div>"), $(".container"),true, "", false, "", false, "row", true);
+        // addIntoDiv($("<div>"), $("#row"),true, "topics", true, "", false, "", false);
     }
 
     function addIntoDiv(divElement, elementToAppendTo ,toAppendBool, attrName, addAttrBool, textToAdd, addTextBool, className, classToAddBool) {
